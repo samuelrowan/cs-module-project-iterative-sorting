4b521bc636fb2b5b4d788094ca5d55805fd0b6de
Samuel Rowan <samuel@samuelrowan.com>
2020-08-13 13:14:52 -0500

    Actually MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,25 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
-
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
-
+    si = 0
+    ei = len(arr)-1
+    while si <= ei:
+        mid = si + (ei - si) // 2
+        midVal = arr[mid]
+        if midVal == target:
+            return mid
+        elif target < midVal:
+            ei = mid - 1
+        else:
+            si = mid + 1
 
     return -1  # not found
